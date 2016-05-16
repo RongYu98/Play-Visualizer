03f3f25a183254ddb0a26911efff41b300e69d4f
igerstein <isaac.gerstein1@gmail.com>
2016-05-16 12:12:53 -0400

    Changed to soccer field

--- front.js
+++ front.js
@@ -1,7 +1,7 @@
 var c = document.getElementById("field");
 var ctx = c.getContext("2d");
 var field = new Image();
-field.src = "static/field.png";
+field.src = "static/field.jpg";
 
 field.onload = function(){
     ctx.drawImage(field,0,0,1000,500);
